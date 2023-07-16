f1f8a3cc5313fb8c31e7e88ad2b7ea39b805f23e
Alexander Shestak <110448322+MorugaShestuck@users.noreply.github.com>
2023-07-16 15:48:30 +0400

    Rename 'bridge_the_islands.js' to 'bridge_the_islands_BadTime.js'

--- js/bridge_the_islands_BadTime.js
+++ js/bridge_the_islands_BadTime.js
@@ -1,31 +1,8 @@
 // https://www.codewars.com/kata/64a815e3e96dec077e305750
 
-class UnionFind {
-    constructor(n) {
-      this.parent = new Array(n).fill(-1);
-    }
-  
-    find(x) {
-      if (this.parent[x] === -1) {
-        return x;
-      }
-      this.parent[x] = this.find(this.parent[x]);
-      return this.parent[x];
-    }
-  
-    union(x, y) {
-      const rootX = this.find(x);
-      const rootY = this.find(y);
-      if (rootX !== rootY) {
-        this.parent[rootX] = rootY;
-      }
-    }
-  }
-  
-  function bridge(islands) {
+function bridge(islands) {
     const n = islands.length;
     const edges = [];
-  
     for (let i = 0; i < n; i++) {
       for (let j = i + 1; j < n; j++) {
         const distance = Math.sqrt(
@@ -35,28 +12,27 @@ class UnionFind {
         edges.push([i, j, distance]);
       }
     }
-  
     edges.sort((a, b) => a[2] - b[2]);
-  
-    const uf = new UnionFind(n);
-  
+    const parent = Array(n).fill(-1);
+    const find = (x) => {
+      if (parent[x] === -1) return x;
+      return find(parent[x]);
+    };
     let totalLength = 0;
     let numEdges = 0;
     for (let i = 0; i < edges.length; i++) {
       const [u, v, distance] = edges[i];
-      const parentU = uf.find(u);
-      const parentV = uf.find(v);
-  
+      const parentU = find(u);
+      const parentV = find(v);
       if (parentU !== parentV) {
-        uf.union(parentU, parentV);
+        parent[parentU] = parentV;
         totalLength += distance;
         numEdges++;
-  
         if (numEdges === n - 1) break;
       }
     }
-  
+
     return totalLength;
-  }
+  }  
 
 console.log(bridge([[0, 0], [1, 0], [1, 1]]));
